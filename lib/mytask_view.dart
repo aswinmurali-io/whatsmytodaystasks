def0deac9d810cb071935396a9f7442d6b5b4b49
aswinmurali-io <aswinmurali.co@gmail.com>
2020-07-29 10:07:09 +0530

    a lot of code cleaning in mytask

--- lib/mytask_view.dart
+++ lib/mytask_view.dart
@@ -10,6 +10,7 @@ import 'package:jiffy/jiffy.dart';
 import 'package:progress_dialog/progress_dialog.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:whatsmytodaystasks/card_design.dart';
+import 'package:whatsmytodaystasks/mytask_view_partials.dart';
 
 import 'custom_dialog.dart' show CustomGradientDialogForm;
 import 'database.dart' show Database;
@@ -106,9 +107,7 @@ class _TaskViewState extends State<TaskView> with SingleTickerProviderStateMixin
                                 tooltip: 'Sync',
                                 icon: const Icon(Icons.sync, size: 17.0),
                                 color: Colors.white,
-                                onPressed: () async {
-                                  await _refreshController.requestRefresh();
-                                }),
+                                onPressed: () async => await _refreshController.requestRefresh()),
                           ),
                         )),
                   Padding(
@@ -123,43 +122,8 @@ class _TaskViewState extends State<TaskView> with SingleTickerProviderStateMixin
                               radius: 23,
                               child: Icon(Icons.account_circle, color: Colors.white),
                               backgroundColor: Colors.red),
-                          onSelected: (email) async {
-                            switch (email) {
-                              case "Add Account":
-                                _accountConnectDialog();
-                                return;
-                              case "Delete Account":
-                                await pr.show();
-                                await Database.deleteAccount();
-                                setState(() => userTasks = {});
-                                await pr.hide();
-                                return;
-                              case "Signout Account":
-                                await pr.show();
-                                await Database.signOut();
-                                setState(() => userTasks = {});
-                                await pr.hide();
-                                return;
-                              case "Switch Google Account":
-                                await pr.show();
-                                await Database.signOut();
-                                userTasks.clear();
-                                await Database.googleAuthDialog();
-                                userTasks = await Database.download();
-                                setState(() => userTasks = userTasks);
-                                await pr.hide();
-                                return;
-                              default:
-                                await pr.show();
-                                await Database.signOut();
-                                userTasks.clear();
-                                await Database.auth(email, Database.getPassword(email), userTasks);
-                                userTasks = await Database.download();
-                                setState(() => userTasks = userTasks);
-                                await pr.hide();
-                                return;
-                            }
-                          },
+                          onSelected: (email) => TaskViewBackend.profileButtonAction(
+                              email, _accountConnectDialog, setState, pr, userTasks),
                           itemBuilder: (context) {
                             return (["Add Account"] + Database.loadAccounts())
                                 .map((String choice) => PopupMenuItem<String>(
@@ -448,70 +412,8 @@ class _TaskViewState extends State<TaskView> with SingleTickerProviderStateMixin
                       Expanded(
                           child: TextField(
                         obscureText: true,
-                        onSubmitted: (_) async {
-                          if (_email != null && _password != null) {
-                            // taken from https://stackoverflow.com/questions/16800540/validate-email-address-in-dart
-                            RegExp __regexEmail = RegExp(
-                                r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-                            if (!__regexEmail.hasMatch(_email)) {
-                              setState2(() => _status = "Enter a valid email address.");
-                              return;
-                            }
-                            if (_password.length < 8) {
-                              setState2(() => _status = "Password should be atleast 8 characters long.");
-                              return;
-                            }
-                            await pr.show();
-                            String errorStatus = await Database.auth(_email, _password, userTasks);
-                            setState2(() => _status = "");
-                            switch (errorStatus) {
-                              case 'ERROR_NETWORK_REQUEST_FAILED':
-                                await pr.hide();
-                                setState2(() => _status = "Request Failed !");
-                                return;
-                              case "ERROR_WRONG_PASSWORD":
-                                await pr.hide();
-                                setState2(() => _status = "Wrong Password, Try again!");
-                                return;
-                              case "ERROR_TOO_MANY_REQUESTS":
-                                await pr.hide();
-                                setState2(() => _status = "Too many requests!");
-                                return;
-                              case "ERROR_USER_NOT_FOUND":
-                              case "auth/user-not-found":
-                                await pr.hide();
-                                showDialog(
-                                    context: context,
-                                    child: AlertDialog(
-                                      content: Text("Account not found. Do you want to create account instead ?"),
-                                      actions: [
-                                        GradientButton(
-                                            elevation: (kIsWeb) ? 0.0 : 5.0,
-                                            child: const Text("Yes"),
-                                            callback: () async {
-                                              await pr.show();
-                                              String error = await Database.register(_email, _password);
-                                              print(error);
-                                              for (int i = 0; i < 3; i++) Navigator.of(context).pop();
-                                              await pr.show();
-                                            }),
-                                        Padding(
-                                          padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                          child: GradientButton(
-                                              elevation: (kIsWeb) ? 0.0 : 5.0,
-                                              child: const Text("No"),
-                                              callback: () => Navigator.of(context).pop()),
-                                        )
-                                      ],
-                                    ));
-                                return;
-                            }
-                            Navigator.of(context).pop();
-                            await pr.hide();
-                            _refreshController.requestRefresh();
-                          } else
-                            setState2(() => _status = "Make sure to fill both, email and password");
-                        },
+                        onSubmitted: (_) async => await TaskViewBackend.accountFormValidation(
+                            _email, _password, setState2, _status, pr, userTasks, context, _refreshController),
                         onChanged: (value) => _password = value,
                         decoration: InputDecoration(hintText: 'Enter password'),
                       )),
@@ -519,76 +421,13 @@ class _TaskViewState extends State<TaskView> with SingleTickerProviderStateMixin
                       Padding(
                         padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
                         child: GradientButton(
-                          elevation: (kIsWeb) ? 0.0 : 5.0,
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [Icon(Icons.account_circle), Text("Connect")]),
-                          increaseWidthBy: 20,
-                          callback: () async {
-                            if (_email != null && _password != null) {
-                              // taken from https://stackoverflow.com/questions/16800540/validate-email-address-in-dart
-                              RegExp __regexEmail = RegExp(
-                                  r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-                              if (!__regexEmail.hasMatch(_email)) {
-                                setState2(() => _status = "Enter a valid email address.");
-                                return;
-                              }
-                              if (_password.length < 8) {
-                                setState2(() => _status = "Password should be atleast 8 characters long.");
-                                return;
-                              }
-                              await pr.show();
-                              String errorStatus = await Database.auth(_email, _password, userTasks);
-                              setState2(() => _status = "");
-                              switch (errorStatus) {
-                                case 'ERROR_NETWORK_REQUEST_FAILED':
-                                  await pr.hide();
-                                  setState2(() => _status = "Request Failed !");
-                                  return;
-                                case "ERROR_WRONG_PASSWORD":
-                                  await pr.hide();
-                                  setState2(() => _status = "Wrong Password, Try again!");
-                                  return;
-                                case "ERROR_TOO_MANY_REQUESTS":
-                                  await pr.hide();
-                                  setState2(() => _status = "Too many requests!");
-                                  return;
-                                case "ERROR_USER_NOT_FOUND":
-                                case "auth/user-not-found":
-                                  await pr.hide();
-                                  showDialog(
-                                      context: context,
-                                      child: AlertDialog(
-                                        content: Text("Account not found. Do you want to create account instead ?"),
-                                        actions: [
-                                          GradientButton(
-                                              elevation: (kIsWeb) ? 0.0 : 5.0,
-                                              child: const Text("Yes"),
-                                              callback: () async {
-                                                await pr.show();
-                                                String error = await Database.register(_email, _password);
-                                                print(error);
-                                                for (int i = 0; i < 3; i++) Navigator.of(context).pop();
-                                                await pr.show();
-                                              }),
-                                          Padding(
-                                            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                            child: GradientButton(
-                                                elevation: (kIsWeb) ? 0.0 : 5.0,
-                                                child: const Text("No"),
-                                                callback: () => Navigator.of(context).pop()),
-                                          )
-                                        ],
-                                      ));
-                                  return;
-                              }
-                              Navigator.of(context).pop();
-                              await pr.hide();
-                              _refreshController.requestRefresh();
-                            } else
-                              setState2(() => _status = "Make sure to fill both, email and password");
-                          },
-                        ),
+                            elevation: (kIsWeb) ? 0.0 : 5.0,
+                            child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [Icon(Icons.account_circle), Text("Connect")]),
+                            increaseWidthBy: 20,
+                            callback: () async => await TaskViewBackend.accountFormValidation(
+                                _email, _password, setState2, _status, pr, userTasks, context, _refreshController)),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(8.0),
@@ -617,6 +456,7 @@ class _TaskViewState extends State<TaskView> with SingleTickerProviderStateMixin
     return false;
   }
 
+  // TODO: this is bad code left for some weird issues, fix this later
   void _tasksEditDialog(
       {bool modifyWhat: false,
       done: false,
@@ -637,16 +477,15 @@ class _TaskViewState extends State<TaskView> with SingleTickerProviderStateMixin
     } else
       dropdown = week2;
 
-    //TODO: remoev hes
-    modifyWhat = modifyWhat;
-    done = done;
-    importance = importance;
-    title = title;
-    description = description;
-    week2 = week2;
-    oldTitle = oldTitle;
-    endtime = endtime;
-    selectedTime = selectedTime;
+    // modifyWhat = modifyWhat;
+    // done = done;
+    // importance = importance;
+    // title = title;
+    // description = description;
+    // week2 = week2;
+    // oldTitle = oldTitle;
+    // endtime = endtime;
+    // selectedTime = selectedTime;
 
     Navigator.of(context).push(PageRouteBuilder(
         opaque: false,
---
+++ lib/mytask_view_partials.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import 'database.dart';
+
+class TaskViewBackend {
+  static profileButtonAction(
+      String email, dynamic _accountConnectDialog, StateSetter setState, ProgressDialog pr, Map userTasks) async {
+    switch (email) {
+      case "Add Account":
+        _accountConnectDialog();
+        return;
+      case "Delete Account":
+        await pr.show();
+        await Database.deleteAccount();
+        setState(() => userTasks = {});
+        await pr.hide();
+        return;
+      case "Signout Account":
+        await pr.show();
+        await Database.signOut();
+        setState(() => userTasks = {});
+        await pr.hide();
+        return;
+      case "Switch Google Account":
+        await pr.show();
+        await Database.signOut();
+        userTasks.clear();
+        await Database.googleAuthDialog();
+        userTasks = await Database.download();
+        setState(() => userTasks = userTasks);
+        await pr.hide();
+        return;
+      default:
+        await pr.show();
+        await Database.signOut();
+        userTasks.clear();
+        await Database.auth(email, Database.getPassword(email), userTasks);
+        userTasks = await Database.download();
+        setState(() => userTasks = userTasks);
+        await pr.hide();
+        return;
+    }
+  }
+
+  static accountFormValidation(String _email, String _password, StateSetter setState2, String _status,
+      ProgressDialog pr, Map userTasks, BuildContext context, RefreshController _refreshController) async {
+    {
+      if (_email != null && _password != null) {
+        // taken from https://stackoverflow.com/questions/16800540/validate-email-address-in-dart
+        RegExp __regexEmail = RegExp(
+            r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+        if (!__regexEmail.hasMatch(_email)) {
+          setState2(() => _status = "Enter a valid email address.");
+          return;
+        }
+        if (_password.length < 8) {
+          setState2(() => _status = "Password should be atleast 8 characters long.");
+          return;
+        }
+        await pr.show();
+        String errorStatus = await Database.auth(_email, _password, userTasks);
+        setState2(() => _status = "");
+        switch (errorStatus) {
+          case 'ERROR_NETWORK_REQUEST_FAILED':
+            await pr.hide();
+            setState2(() => _status = "Request Failed !");
+            return;
+          case "ERROR_WRONG_PASSWORD":
+            await pr.hide();
+            setState2(() => _status = "Wrong Password, Try again!");
+            return;
+          case "ERROR_TOO_MANY_REQUESTS":
+            await pr.hide();
+            setState2(() => _status = "Too many requests!");
+            return;
+          case "ERROR_USER_NOT_FOUND":
+          case "auth/user-not-found":
+            await pr.hide();
+            showDialog(
+                context: context,
+                child: AlertDialog(
+                  content: Text("Account not found. Do you want to create account instead ?"),
+                  actions: [
+                    GradientButton(
+                        elevation: (kIsWeb) ? 0.0 : 5.0,
+                        child: const Text("Yes"),
+                        callback: () async {
+                          await pr.show();
+                          String error = await Database.register(_email, _password);
+                          print(error);
+                          for (int i = 0; i < 3; i++) Navigator.of(context).pop();
+                          await pr.show();
+                        }),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                      child: GradientButton(
+                          elevation: (kIsWeb) ? 0.0 : 5.0,
+                          child: const Text("No"),
+                          callback: () => Navigator.of(context).pop()),
+                    )
+                  ],
+                ));
+            return;
+        }
+        Navigator.of(context).pop();
+        await pr.hide();
+        _refreshController.requestRefresh();
+      } else
+        setState2(() => _status = "Make sure to fill both, email and password");
+    }
+  }
+}
